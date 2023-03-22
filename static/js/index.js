fd66b99257518beb6621956c0bf3af43a36eb96f
LeonardSx <MatheoJE_2000@hotmail.com>
2023-03-21 19:59:45 -0500

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,94 +1,70 @@
-/*
-window.userWalletAddress = null;
-const loginButton = document.getElementById("connect-metamask-btn");
-const titulowallet = document.getElementById("titulo_wallet");
-function toggleButtonLogin() {
+const loginButton = document.getElementById("connect_metamask");
+const walletTitle = document.getElementById("titulo_wallet");
+
+const API_URL = "<%= process.env.HOST %>/auth/v1/login";
+const DEFAULT_REFERRER_ID = "<%= referidoId %>";
+const ERROR_META_MASK_NOT_INSTALLED = "MetaMask is not installed";
+
+const toggleLoginButton = () => {
+  toggleButton(loginWithMetaMask);
+};
+
+const toggleSignOutButton = () => {
+  toggleButton(signOutOfMetaMask);
+};
+
+const toggleButton = (action) => {
   if (!window.ethereum) {
-    titulowallet.innerText = "MetaMask is not installed";
-    loginButton.classList.add(
-      "bg-gray-500",
-      "text-gray-100",
-      "cursor-not-allowed"
-    );
+    walletTitle.innerText = ERROR_META_MASK_NOT_INSTALLED;
+    loginButton.classList.add("bg-gray-500", "text-gray-100", "cursor-not-allowed");
     return false;
   }
-  loginButton.addEventListener("click", loginWithMetaMask);
-}
-
-function toggleButtonsignOut() {
-  if (!window.ethereum) {
-    titulowallet.innerText = "MetaMask is not installed";
-    loginButton.classList.add(
-      "bg-gray-500",
-      "text-gray-100",
-      "cursor-not-allowed"
-    );
-    return false;
-  }
-  loginButton.addEventListener("click", signOutOfMetaMask);
-}
-
-async function loginWithMetaMask() {
-  const accounts = await window.ethereum
-    .request({ method: "eth_requestAccounts" })
-    .catch((e) => {
-      console.error(e.message);
-      return;
-    });
-  if (!accounts) {
-    return;
-  }
-
-  window.userWalletAddress = accounts[0];
-  titulowallet.innerText = "Sign out";
-
-  const request = await fetch("<%= process.env.HOST %>/auth/v1/login", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      wallet: window.userWalletAddress,
-      referred: "<%= referidoId %>",
-    }),
-  });
-  response = request.text();
-  window.location = "/inventory";
   loginButton.removeEventListener("click", loginWithMetaMask);
-  setTimeout(() => {
-    loginButton.addEventListener("click", signOutOfMetaMask);
-  }, 200);
-}
-function redireccionarPagina() {
-  window.location = "/";
-}
-function signOutOfMetaMask() {
-  window.userWalletAddress = null;
-  titulowallet.innerText = "Connect Wallet";
-  window.location = "/logout";
   loginButton.removeEventListener("click", signOutOfMetaMask);
-  setTimeout(() => {
-    loginButton.addEventListener("click", loginWithMetaMask);
-  }, 200);
-}
+  loginButton.addEventListener("click", action);
+};
 
-const ajax = async (config) => {
-  const request = await fetch(config.url, {
-    method: config.method,
+const loginWithMetaMask = async () => {
+  try {
+    const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+    const userWalletAddress = accounts[0];
+    walletTitle.innerText = "Sign out";
+    await ajax({
+      url: API_URL,
+      method: "POST",
+      payload: {
+        wallet: userWalletAddress,
+        referred: DEFAULT_REFERRER_ID,
+      },
+    });
+    window.location = "/inventory";
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const signOutOfMetaMask = () => {
+  window.location = "/logout";
+  walletTitle.innerText = "Connect Wallet";
+  toggleButton(loginWithMetaMask);
+};
+
+const ajax = async ({ url, method, payload }) => {
+  const response = await fetch(url, {
+    method,
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(config.payload),
+    body: JSON.stringify(payload),
   });
-  response = await request.json();
+  return await response.json();
+};
+
+const redirectToHomePage = () => {
+  window.location = "/";
 };
 
 window.addEventListener("DOMContentLoaded", () => {
-  toggleButtonLogin();
+  toggleLoginButton();
 });
-
-*/
-  // Obtener todos los botones de los acordeones
- 
